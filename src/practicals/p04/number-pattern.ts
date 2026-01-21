37e3b81d244fac2b9b5f59c13b141c60e689133d
bananabank555 <watnirun03@gmail.com>
2026-01-21 16:49:37 +0700

    1111

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,11 @@
 // Write your code below
+const args = process.argv.slice(2);
+const size = parseInt(args[0]);
+
+if (!isNaN(size) && size > 0) {
+    for (let i = 0; i < size; i++) {
+        const count = size + i;
+        const row = Array.from({ length: count }, (_, j) => count + j).join("");
+        console.log(row);
+    }
+}
