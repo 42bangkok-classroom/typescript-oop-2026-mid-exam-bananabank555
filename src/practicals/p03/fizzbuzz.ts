b3b9c0d491a1d707fa5ab98e412b79aeae4b3e2e
bananabank555 <watnirun03@gmail.com>
2026-01-21 16:36:17 +0700

    1111

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -2,14 +2,12 @@
 export{};
 const arg = process.argv[2];
 const n = Number(arg);
-if (arg === undefined || Number.isNaN(n) || n<=0)console.log("invalid.input");
+if (arg === undefined || Number.isNaN(n)|| n<=0)console.log("nothing");
 for (let i = 1;i<=n;i++){
-    if(i % 15 === 0)console.log("Correct");
-    else if(i % 30 === 0)console.log("Correct");
-    else if(i % 50 === 0)console.log("Correct");
-    else if(i % 70 === 0)console.log("Correct");
-    else if(i % 100 === 0)console.log("Correct");
-    else if(i % 150 === 0)console.log("Correct");
-    else if(i % 50 === 0)console.log("Correct");
-    else console.log(i);
+    if(i = 15)console.log("Correct");
+    else if(i = 30)console.log("Correct");
+    else if(i = 50)console.log("Correct");
+    else if(i = 70)console.log("Correct");
+    else if(i = 100)console.log("Correct");
+    else if(i = 150)console.log("Correct");
 }
