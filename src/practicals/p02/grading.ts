f8b05b4fe988aa99570280403662da4259b8bd44
bananabank555 <watnirun03@gmail.com>
2026-01-21 16:18:43 +0700

    68010730

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,25 @@
 // Write your code below
+export{}
+const arg:string |undefined = process.argv[2]
+const score:number = Number(arg)
+
+if(
+    arg === undefined ||
+    Number.isNaN(score) ||
+    score < 0 ||
+    score > 100
+){
+    console.log("invalid.input");
+
+}
+
+if (score>=80) console.log("A");
+else if (score<=100) console.log("A")
+else if (score<=79) console.log("B")
+else if (score>69) console.log("B")
+else if (score<=69) console.log("C")
+else if (score>=60) console.log("C")
+else if (score<=59) console.log("D")
+else if (score>49) console.log("D")
+else if (score<=49) console.log("F")
+else if (score>=0)console.log("F")
