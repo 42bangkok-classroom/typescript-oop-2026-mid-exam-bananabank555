9750adb5312fdad1134501943dd9b017fee682ce
bananabank555 <watnirun03@gmail.com>
2026-01-21 16:59:58 +0700

    68010730

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -14,12 +14,12 @@ if(
 }
 
 if (score>=80) console.log("A");
-else if (score<=100) console.log("A")
-else if (score<=79) console.log("B")
-else if (score>69) console.log("B")
-else if (score<=69) console.log("C")
-else if (score>=60) console.log("C")
-else if (score<=59) console.log("D")
-else if (score>49) console.log("D")
-else if (score<=49) console.log("F")
-else if (score>=0)console.log("F")
+else if (score<=100) console.log("A");
+else if (score<=79) console.log("B");
+else if (score>69) console.log("B");
+else if (score<=69) console.log("C");
+else if (score>=60) console.log("C");
+else if (score<=59) console.log("D");
+else if (score>49) console.log("D");
+else if (score<=49) console.log("F");
+else if (score>=0)console.log("F");
