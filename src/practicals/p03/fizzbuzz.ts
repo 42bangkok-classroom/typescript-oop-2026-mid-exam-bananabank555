3de13734deedf802aa1e0a12704a4ddfbb76d408
bananabank555 <watnirun03@gmail.com>
2026-01-21 15:53:31 +0700

    1111

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,1 +1,11 @@
 // Write your code below
+export{};
+const arg = process.argv[2];
+const n = Number(arg);
+if (arg === undefined || Number.isNaN(n) || n<=0)console.log("invalid.input");
+for (let i = 1;i<=n;i++){
+    if(i % 5 === 0)console.log("FizzBuzz");
+    else if(i % 3 === 0)console.log("Fizz");
+    else if(i % 5 === 0)console.log("Buzz");
+    else console.log(i);
+}
